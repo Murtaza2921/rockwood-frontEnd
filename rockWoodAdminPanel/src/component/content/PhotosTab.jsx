1e46128c5afe1ca6f1531ac5898ffe38affd63d1
Asif Raza <asif.raza@bytedash.my>
2023-12-04 17:55:19 +0500

    PhotosTab updated

--- rockWoodAdminPanel/src/component/content/PhotosTab.jsx
+++ rockWoodAdminPanel/src/component/content/PhotosTab.jsx
@@ -4,8 +4,9 @@ import { FaTrash } from 'react-icons/fa';
 const PhotosTab = ({ isSidebarOpen }) => {
   const [images, setImages] = useState([]);
   const [selectedImages, setSelectedImages] = useState([]);
-  const [renameMode, setRenameMode] = useState(false);
+  const [isRenameMode, setIsRenameMode] = useState(false);
   const [newImageName, setNewImageName] = useState('');
+  const [selectedImageIndex, setSelectedImageIndex] = useState(null);
 
   const handleImageChange = (event) => {
     const files = event.target.files;
@@ -20,7 +21,7 @@ const PhotosTab = ({ isSidebarOpen }) => {
           reader.onloadend = () => {
             resolve({
               src: reader.result,
-              name: renameMode ? (newImageName || file.name) : file.name,
+              name: newImageName || file.name,
             });
           };
           reader.readAsDataURL(file);
@@ -29,8 +30,9 @@ const PhotosTab = ({ isSidebarOpen }) => {
     ).then((loadedImages) => {
       setImages([...images, ...loadedImages]);
       setSelectedImages([]);
-      setRenameMode(false);
+      setIsRenameMode(false);
       setNewImageName('');
+      setSelectedImageIndex(null);
     });
   };
 
@@ -38,6 +40,19 @@ const PhotosTab = ({ isSidebarOpen }) => {
     const updatedImages = [...images];
     updatedImages.splice(index, 1);
     setImages(updatedImages);
+    setIsRenameMode(false);
+    setNewImageName('');
+    setSelectedImageIndex(null);
+  };
+
+  const handleRenameClick = (index) => {
+    setIsRenameMode(true);
+    setSelectedImageIndex(index);
+    setNewImageName(images[index]?.name || '');
+  };
+
+  const handleRenameChange = (event) => {
+    setNewImageName(event.target.value);
   };
 
   return (
@@ -54,32 +69,41 @@ const PhotosTab = ({ isSidebarOpen }) => {
 
       {selectedImages.length > 0 && (
         <div className="mb-4">
-          <label className="flex items-center">
-            <input
-              type="checkbox"
-              checked={renameMode}
-              onChange={() => setRenameMode(!renameMode)}
-              className="mr-2"
-            />
-            <span className="text-sm">Enable Rename</span>
-          </label>
-
-          {renameMode && (
-            <input
-              type="text"
-              placeholder="Enter image name"
-              value={newImageName}
-              onChange={(e) => setNewImageName(e.target.value)}
-              className="mt-2 px-2 py-1 border rounded"
-            />
-          )}
-
           <button
             onClick={handleImageUpload}
-            className="mt-2 bg-blue-500 text-white px-4 py-2 rounded"
+            className="mr-2 bg-blue-500 text-white px-4 py-2 rounded"
           >
             Upload Images
           </button>
+          {isRenameMode ? (
+            <div className="inline-block">
+              <input
+                type="text"
+                placeholder="Enter image name"
+                value={newImageName}
+                onChange={handleRenameChange}
+                className="px-2 py-1 border rounded"
+              />
+              <button
+                onClick={() => {
+                  handleImageUpload();
+                  setIsRenameMode(false);
+                  setNewImageName('');
+                  setSelectedImageIndex(null);
+                }}
+                className="bg-blue-500 text-white px-4 py-2 rounded"
+              >
+                Save
+              </button>
+            </div>
+          ) : (
+            <button
+              onClick={() => setIsRenameMode(true)}
+              className="bg-blue-500 text-white px-4 py-2 rounded"
+            >
+              Rename
+            </button>
+          )}
         </div>
       )}
 
@@ -89,22 +113,32 @@ const PhotosTab = ({ isSidebarOpen }) => {
           <div key={index} className="bg-white border rounded overflow-hidden">
             <img src={image.src} alt={image.name} className="w-full h-48 object-cover" />
             <div className="p-4">
-              {renameMode && (
+              {isRenameMode && selectedImageIndex === index ? (
                 <input
                   type="text"
                   placeholder="Enter image name"
-                  value={image.name}
-                  onChange={(e) => handleImageNameChange(e, index)}
+                  value={newImageName}
+                  onChange={handleRenameChange}
                   className="mt-2 px-2 py-1 border rounded w-full"
                 />
+              ) : (
+                <p className="font-semibold">{image.name}</p>
               )}
-              <button
-                onClick={() => handleRemoveImage(index)}
-                className="mt-2 bg-red-500 text-white px-4 py-2 rounded-full flex items-center"
-              >
-                <FaTrash className="mr-2" />
-                Remove
-              </button>
+              <div className="flex">
+                <button
+                  onClick={() => (isRenameMode ? handleImageUpload() : handleRenameClick(index))}
+                  className="mr-2 bg-blue-500 text-white px-4 py-2 rounded"
+                >
+                  {isRenameMode ? 'Save' : 'Rename'}
+                </button>
+                <button
+                  onClick={() => handleRemoveImage(index)}
+                  className="bg-red-500 text-white px-4 py-2 rounded"
+                >
+                  <FaTrash className="mr-2" />
+                  Remove
+                </button>
+              </div>
             </div>
           </div>
         ))}
